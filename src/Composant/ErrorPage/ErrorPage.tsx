52083f5ae856040a49283180829ccb2d4aa792d6
D-D-f <defreitas-david@outlook.fr>
2023-11-24 20:53:01 +0100

    add error page

--- src/Composant/ErrorPage/ErrorPage.tsx
+++ src/Composant/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p></p>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <p>
+        <NavLink to="/">Retouner sur la page d'accueil</NavLink>{" "}
+      </p>
     </div>
   );
 };
--- src/Composant/Path/Path.tsx
+++ src/Composant/Path/Path.tsx
@@ -2,17 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import PageLocation from "../PageLocation/PageLocation";
 import About from "../About/About";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 interface PathProps {
   data: [];
 }
 
 const Path = ({ data }: PathProps) => {
+  console.log("Current path: ", window.location.pathname);
   return (
     <Routes>
       <Route path="/" element={<Home data={data} />} />
       <Route path="/:id" element={<PageLocation data={data} />} />
       <Route path="/about" element={<About />} />
+      <Route path="/error" element={<ErrorPage />} />
     </Routes>
   );
 };
